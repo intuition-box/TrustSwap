d7878fed48a07db362320348071a8b2550e1e9cd
Florian-THP <triboutflorian@outlook.com>
2025-08-27 08:57:30 +0200

    change config to intuit ts chain

--- frontend/src/chain/intuit.ts
+++ frontend/src/chain/intuit.ts
@@ -1,20 +1,21 @@
-import { Chain } from 'wagmi/chains'
+import { Chain } from '@rainbow-me/rainbowkit'
 
 export const intuitChain: Chain = {
   id: 13579,
   name: 'Intuition Testnet',
-  network: 'intuition-testnet',
+  iconUrl: 'https://example.com/icon.png', // remplace par l'URL de ton icône
+  iconBackground: '#000000',               // couleur de fond de l’icône
   nativeCurrency: {
-    decimals: 18,
     name: 'Test Trust',
     symbol: 'tTRUST',
+    decimals: 18,
   },
   rpcUrls: {
     default: { http: ['https://testnet.rpc.intuition.systems/http'] },
     public: { http: ['https://testnet.rpc.intuition.systems/http'] },
   },
   blockExplorers: {
-    default: { name: 'Explorer', url: 'https://explorer.intuition.systems' }, // facultatif si pas d’explorer
+    default: { name: 'Explorer', url: 'https://explorer.intuition.systems' },
   },
   testnet: true,
-}
+} as const
