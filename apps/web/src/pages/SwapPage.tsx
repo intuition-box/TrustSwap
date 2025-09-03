39cc9858667ac8583e1fdb20a84b3be255dafcaa
Florian-THP <triboutflorian@outlook.com>
2025-09-03 22:58:37 +0200

    styles(config): update vite.config.ts and tsconfig.json for CSS alias

--- apps/web/src/pages/SwapPage.tsx
+++ apps/web/src/pages/SwapPage.tsx
@@ -1,12 +1,13 @@
+import styles from "@ui/styles/Swap.module.css";
+
+
 export default function SwapPage() {
-  // Tu pourras brancher ici ton composant Swap existant
+
   return (
-    <section className="grid gap-4">
-      <h1 className="text-2xl font-semibold">Swap</h1>
-      <div className="rounded-2xl border border-white/10 p-4">
-        {/* <Swap /> */}
-        <p className="opacity-80">Interface de swap à brancher ici.</p>
-      </div>
+    <section>
+     <div className={styles.swapCard}>
+
+     </div>
     </section>
   );
 }
