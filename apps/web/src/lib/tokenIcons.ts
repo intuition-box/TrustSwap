ef7a7697ff4ed006cabe83ee9bed3c160f34000a
Florian-THP <triboutflorian@outlook.com>
2025-09-05 10:24:22 +0200

    style(swap) add css and anim to tokenSelector

---
+++ apps/web/src/lib/tokenIcons.ts
@@ -0,0 +1,6 @@
+// tokenIcons.ts
+import WtrustIcon from "../assets/trust.png";
+
+export const tokenIcons: Record<string, string> = {
+  "0xc82d6A5e0Da8Ce7B37330C4D44E9f069269546E6": WtrustIcon, // attention majuscule
+};
